c713d7e7021fa454762533510b69910a4f702112
lcech <ja@lukascech.cz>
2023-05-15 17:34:26 +0200

    close feauture

--- bookmarklet.js
+++ bookmarklet.js
@@ -8,14 +8,20 @@ javascript:(function(){
   iframe.style.height = '100%';
   iframe.style.background = 'White';
   iframe.style.zIndex = 100;
+  iframe.id = 'payload-inspector';
 
-  iframe.addEventListener("load", function() {
+  iframe.addEventListener('load', function() {
     iframe.contentWindow.postMessage(
       document.querySelector('.PayloadModal_payload__jZjnP').innerText,
       '*'
     );
   });
 
+  window.addEventListener('keydown', function(event) {
+    if (event.isComposing || event.key === 'Escape') {
+      document.body.removeChild(iframe);
+    }
+  }, { once: true });
+
   document.body.appendChild(iframe);
-  return void(0);
 })();
